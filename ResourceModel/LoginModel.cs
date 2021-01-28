c5357a7eef2f3319210a4053393f70631b032349
Marianne Recuenco <marianne.recuenco@gmail.com>
2021-01-28 18:18:02 +0800

    Update to remove unused controller

---
+++ ResourceModel/LoginModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Plantophile.ResourceModel
+{
+    public class LoginModel
+    {
+        [Required]
+        [EmailAddress]
+        public string EmailAddress { get; set; }
+
+        public string PlainTextPassword { get; set; }
+    }
+}
